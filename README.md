8392e3f9c019181d293c688adf98ffe6704e9113
joirneto <joirneto@gmail.com>
2021-07-13 16:00:17 -0300

    Parte 3 ok

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # metions-api
- 
+
+Aprendemos a criar uma API com Node.js, Express, MongoDB e o Atlas, utilizando Postman para validação se está tudo funcionando
+organizar nosso código utilizando repository, construímos nossos métodos de inserção e listagem.
--- src/controllers/metions-constroller.js
+++ src/controllers/metions-constroller.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const Metions = mongoose.model('Metions');
+const { validationResult } = require('express-validator');
+const repository = require('../repositories/metions-repository');
+
 
 //list
 exports.listMetions = async (req, res) => {
   try {
-    const data = await Metions.find({});
+    const data = await repository.listMetions();
     res.status(200).send(data);
   } catch (e) {
     res.status(500).send({message: 'Falha ao carregar as menções.'});
@@ -13,17 +14,53 @@ exports.listMetions = async (req, res) => {
 
 //create
 exports.createMetions = async (req, res) => {
+  const {errors} = validationResult(req);
+
+  if(errors.length >0) {
+    return res.status(400).send({message: errors});
+  }
+
   try {
-    const metion = new Metions ({
+    const metion = repository.createMetions ({
       friend: req.body.friend,
       metion: req.body.metion
-    });
-    console.log(metion);
-
-    await metion.save();
-    
+    });   
     res.status(201).send({message: 'Menção cadastrada com sucesso.'})
   }catch(e){
     res.status(500).send({message: 'Falha ao cadastrar a menção.'})
   }
 };
+
+//Update
+exports.updateMetion = async (req, res) => {
+  const {errors} = validationResult(req);
+
+  if(errors.length >0) {
+    return res.status(400).send({message: errors});
+  }
+  
+  try {
+    await repository.updateMetion(req.params.id, req.body);
+    res.status(200).send({
+      message: 'Menção atualizada com sucesso!'
+    })
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao atualizar a menção.'
+    })
+  }
+}
+
+//Delete
+exports.deleteMetion = async (req, res) => {
+  try {
+    await repository.deleteMetion(req.params.id);
+    res.status(200).send({
+      message: 'Menção removida com sucesso!'
+    })
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao remover a menção!'
+    })
+  }
+}
